601bb38abdb34f85f818557f6a65e67ae94530fe
Danish Hakim <danish.hakim04@gmail.com>
2025-01-15 01:41:50 +0800

    Code(catalog): Add price and colour filter

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,11 +1,11 @@
-import { getProducts, createCard, getProductsByCategory, getQueryParams, addQueryParams, loadProducts, compileAvailableSizes, getProductsBySize } from './utility.js';
+import { getProducts, createCard, getProductsByCategory, getQueryParams, getProductsByColour, addQueryParams, loadProducts, compileAvailableSizes, getProductsBySize, compileAvailableColours, getProductsByColourWithIndex, loadProductsWithColour, getProductByMinPrice, getProductByMaxPrice } from './utility.js';
 
 function loadCategoryButtons() {
     var buttons = document.querySelectorAll('.filter-item');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             var category = button.querySelector('p').textContent;
-            addQueryParams('category', category);
+            addQueryParams([{ "name": 'category', "value": category }]);
         });
     });
 }
@@ -26,36 +26,81 @@ function loadSizes(sizes) {
                 console.log(error);
             }
             div.classList.add('active');
-            addQueryParams('size', size);
+            addQueryParams([{ "name": 'size', "value": size }]);
         });
 
         document.getElementById('size-filter').appendChild(div).appendChild(p);
     });
 }
 
+function loadColours(colours) {
+    colours.forEach((colour, index) => {
+        var div = document.createElement('div');
+        var p = document.createElement('p');
+
+        div.setAttribute('class', 'size');
+        p.textContent = colour;
+        div.setAttribute('id', colour);
+
+        div.addEventListener('click', () => {
+            try {
+                document.querySelector('.sizes.active').classList.remove('active');
+            } catch (error) {
+                console.log(error);
+            }
+            div.classList.add('active');
+            addQueryParams([{ "name": 'colour', "value": colour }]);
+        });
+
+        document.getElementById('colour-filter').appendChild(div).appendChild(p);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
-    var category = getQueryParams('category');
-    var size = getQueryParams('size');
     const searchParams = new URLSearchParams(window.location.search);
     var hasQueryParams = searchParams.size === 0 ? false : true;
     var products = await getProducts();
     if (hasQueryParams) {
-        document.getElementById(category.toLowerCase().replace(" ", "-").replace(" ", "-")).classList.add('active');
-        document.getElementById('page-title').textContent = category;
         var sizes = compileAvailableSizes(products).sort();
+        var colours = compileAvailableColours(products).sort();
         loadSizes(sizes);
+        loadColours(colours);
+        loadPriceRangeSliders();
         products = await filterProducts(products, searchParams);
-        loadProducts(products);
-        try {
-            console.log("Test")
+        if (searchParams.has('category')) {
+            var category = getQueryParams('category');
+            document.getElementById(category.toLowerCase().replace(" ", "-").replace(" ", "-")).classList.add('active');
+            document.getElementById('page-title').textContent = category;
+        }
+        if (searchParams.has('size')) {
+            var size = getQueryParams('size');
             document.getElementById(size).classList.add('active');
-        } catch (error) {
-            console.log('Invalid size query parameter:', size);
+        }
+        if (searchParams.has('colour')) {
+            var colour = getQueryParams('colour');
+            document.getElementById(colour).classList.add('active');
+            var productsWithColours = await getProductsByColourWithIndex(products, colour);
+        }
+        if (searchParams.has('price-min')) {
+            var priceMin = getQueryParams('price-min');
+            updatePriceMinValue(priceMin);
+        }
+        if (searchParams.has('price-max')) {
+            var priceMax = getQueryParams('price-max');
+            updatePriceMaxValue(priceMax);
+        }
+        if (searchParams.has('colour')) {
+            loadProductsWithColour(products, productsWithColours);
+        } else {
+            loadProducts(products);
         }
     } else {
         loadProducts(products);
         var sizes = compileAvailableSizes(products).sort();
+        var colours = compileAvailableColours(products).sort();
         loadSizes(sizes);
+        loadColours(colours);
+        loadPriceRangeSliders();
     }
     loadCategoryButtons();
 })
@@ -70,11 +115,61 @@ function filterProducts(products, searchParams) {
                 case 'size':
                     products = await getProductsBySize(products, value);
                     break;
+                case 'colour':
+                    products = await getProductsByColour(products, value);
+                    break;
+                case 'price-min':
+                    products = await getProductByMinPrice(products, value);
+                    break;
+                case 'price-max':
+                    products = await getProductByMaxPrice(products, value);
+                    break;
                 default:
                     console.log('Invalid query parameter:', key);
                     break;
             }
         }
+        console.log(products);
         resolve(products);
     })
+}
+
+function loadPriceRangeSliders() {
+    var priceMinRange = document.getElementById('price-min');
+    var priceMaxRange = document.getElementById('price-max');
+
+    priceMinRange.addEventListener('input', () => {
+        updatePriceMinValue(priceMinRange.value);
+    });
+
+    priceMaxRange.addEventListener('input', () => {
+        updatePriceMaxValue(priceMaxRange.value);
+    });
+
+    updatePriceMaxValue(priceMaxRange.value);
+    updatePriceMinValue(priceMinRange.value);
+
+    var applyButton = document.getElementById('price-filter-button');
+    applyButton.addEventListener('click', () => {
+        addQueryParams([{ "name": 'price-min', "value": priceMinRange.value }, { "name": 'price-max', "value": priceMaxRange.value }]);
+    });
+}
+
+//helper functions for price filtering
+function updatePriceMinValue(value) {
+    document.getElementById('price-min-value').textContent = `RM ${value}`;
+    updatePriceMinFormValue(value);
+}
+
+function updatePriceMinFormValue(value) {
+    document.getElementById('price-min').value = value;
+}
+
+function updatePriceMaxFormValue(value) {
+    document.getElementById('price-max').value = value;
+}
+
+function updatePriceMaxValue(value) {
+    document.getElementById('price-max-value').textContent = `RM ${value}`;
+    updatePriceMaxFormValue(value);
 }
--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,6 +1,7 @@
 const API_PATH = `${window.location.origin}/api`;
 const TEXT_BOLD = "text bold";
 const TEXT_GREY = "text grey";
+const COLOURS = ["Black", "Blue", "Brown", "Green", "Orange", "Pink", "Purple", "Red", "White", "Yellow"];
 
 export function fetchJSON(jsonFilePath) {
     return new Promise((resolve, reject) => {
@@ -51,6 +52,42 @@ export function createCard(product) {
     catalog.appendChild(card);
 }
 
+export function createCardWithColour(product, colorIndex) {
+    var card = document.createElement('div');
+    var img = document.createElement('img');
+    var title = document.createElement('p');
+    var category = document.createElement('p');
+    var color = document.createElement('p');
+    var prices = document.createElement('p');
+
+    card.classList.add("card");
+    console.log(product);
+    console.log(colorIndex);
+    img.setAttribute("src", product.images[colorIndex][0]);
+    img.setAttribute("alt", product.title);
+    title.setAttribute("class", TEXT_BOLD);
+    category.setAttribute("class", TEXT_GREY);
+    color.setAttribute("class", TEXT_GREY);
+    prices.setAttribute("class", TEXT_BOLD);
+
+    title.textContent = product.title;
+    category.textContent = `${product.category} Shoes`;
+    color.textContent = `${product.numOfColours} Colours`;
+    prices.textContent = `RM ${product.price.toFixed(2)}`;
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(category);
+    card.appendChild(color);
+    card.appendChild(prices);
+
+    card.addEventListener('click', () => {
+        window.location.href = `../product/?id=${product.id}&color=${colorIndex}`;
+    });
+
+    var catalog = document.getElementById("catalog");
+    catalog.appendChild(card);
+}
+
 export function getProducts() {
     return new Promise((resolve, reject) => {
         fetchJSON(`${API_PATH}/product/product.json`)
@@ -82,20 +119,76 @@ export function getProductsById(products, id) {
     });
 }
 
+export function getProductByMinPrice(products, minPrice) {
+    return new Promise((resolve, reject) => {
+        resolve(products.filter(product => product.price >= minPrice));
+    });
+}
+
+export function getProductByMaxPrice(products, maxPrice) {
+    return new Promise((resolve, reject) => {
+        resolve(products.filter(product => product.price <= maxPrice));
+    });
+}
+
+export function getProductsByColour(products, colour) {
+    return new Promise((resolve, reject) => {
+        const lowerCaseColour = colour.toLowerCase();
+        resolve(products.filter(product =>
+            product.colours.some(colourObj =>
+                colourObj.colour.some(c =>
+                    c.toLowerCase().includes(lowerCaseColour)
+                )
+            )
+        ));
+    });
+}
+
+export function getProductsByColourWithIndex(products, colour) {
+    return new Promise((resolve, reject) => {
+        const lowerCaseColour = colour.toLowerCase();
+        const filteredProducts = products.map(product => {
+            const matchingColourIndex = product.colours.findIndex(colourObj =>
+                colourObj.colour.some(c =>
+                    c.toLowerCase().includes(lowerCaseColour)
+                )
+            );
+            if (matchingColourIndex !== -1) {
+                return {
+                    ...product,
+                    matchingColourIndex
+                };
+            }
+            return null;
+        }).filter(product => product !== null);
+        resolve(filteredProducts);
+    });
+}
+
 export function getQueryParams(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
 
-export function addQueryParams(name, value) {
+export function addQueryParams(params) {
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set(name, value);
+    params.forEach(item => {
+        urlParams.set(item.name, item.value);
+    });
     window.location.search = urlParams.toString();
 }
 
 export function loadProducts(products) {
     products.forEach(product => {
         createCard(product);
+    });
+}
+
+export function loadProductsWithColour(products, colorIndexArray) {
+    products.forEach(product => {
+        const colorIndexObj = colorIndexArray.find(item => item.id === product.id);
+        const colorIndex = colorIndexObj ? colorIndexObj.matchingColourIndex : 0;
+        createCardWithColour(product, colorIndex);
     });
 }
 
@@ -115,4 +208,8 @@ export function compileAvailableSizes(products) {
     });
     console.log(sizes);
     return sizes;
+}
+
+export function compileAvailableColours() {
+    return COLOURS;
 }
