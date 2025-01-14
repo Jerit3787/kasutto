de281570c83dfa4bac2084d46bb4efd18a73c386
Danish Hakim <danish.hakim04@gmail.com>
2025-01-15 04:23:18 +0800

    Code: add incomplete complete page
    raaaaahhh

---
+++ assets/js/complete.js
@@ -0,0 +1,32 @@
+import { getProductsById, createPaymentCard, getProducts } from './utility.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadCart();
+});
+
+async function loadCart() {
+    var cart = JSON.parse(localStorage.getItem('cart'));
+    console.log(cart);
+
+    createCart(cart)
+}
+
+function updateTotal(total) {
+    document.getElementById('total').textContent = `RM ${total.toFixed(2)}`;
+    document.getElementById('subtotal').textContent = `RM ${total.toFixed(2)}`;
+}
+
+function createCart(cart) {
+    return new Promise((resolve, reject) => {
+        var total = 0;
+        cart.forEach(async (item, index) => {
+            var products = await getProducts();
+            var productData = await getProductsById(products, item.id);
+            console.log(productData);
+            createPaymentCard(productData[0], parseInt(item.color), item.size, item.quantity, index);
+            console.log(productData[0].price);
+            total += productData[0].price * item.quantity;
+        });
+        resolve(total);
+    })
+}
